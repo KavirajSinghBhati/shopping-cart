c5a0af3d6dcca7af0370fa391bd22904d205f3dc
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-08-12 20:08:43 +0530

    project done

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
+import React, { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import axios from 'axios';
 import { useShoppingCart } from '../context/cart.context';
 import CartItem from '../components/CartItem';
 import { formatCurrency } from '../utils/helper';
@@ -8,6 +8,8 @@ import storeItems from '../assets/data.json';
 
 const CartPage = () => {
   const { cartItems, cartQuantity } = useShoppingCart();
+  const [loading, setLoading] = useState(false);
+  const [orderAmount, setOrderAmount] = useState(0);
   if (cartItems.length === 0) {
     return (
       <div className='col-md-4 order-md-2 mb-4'>
@@ -17,6 +19,67 @@ const CartPage = () => {
       </div>
     );
   }
+
+  function loadRazorpay() {
+    const amount = cartItems.reduce((total, cartItem) => {
+      const item = storeItems.find((i) => i.id === cartItem.id);
+      return total + (item?.price || 0) * cartItem.quantity;
+    }, 0);
+    setOrderAmount(amount);
+    const script = document.createElement('script');
+    script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+    script.onerror = () => {
+      alert('Razorpay SDK failed to load. Are you online?');
+    };
+    script.onload = async () => {
+      try {
+        setLoading(true);
+        const result = await axios.post('/create-order', {
+          amount: orderAmount + '00',
+        });
+        const { amount, id: order_id, currency } = result.data;
+        const {
+          data: { key: razorpayKey },
+        } = await axios.get('/get-razorpay-key');
+        const options = {
+          key: razorpayKey,
+          amount: amount.toString(),
+          currency: currency,
+          name: 'example name',
+          description: 'example transaction',
+          order_id: order_id,
+          handler: async function (response) {
+            const result = await axios.post('/pay-order', {
+              amount: amount,
+              razorpayPaymentId: response.razorpay_payment_id,
+              razorpayOrderId: response.razorpay_order_id,
+              razorpaySignature: response.razorpay_signature,
+            });
+            alert(result.data.msg);
+          },
+          prefill: {
+            name: 'John Doe',
+            email: 'email@example.com',
+            contact: '1111111111',
+          },
+          notes: {
+            address: 'Jodhpur, Rajasthan',
+          },
+          theme: {
+            color: '#80c0f0',
+          },
+        };
+        setLoading(false);
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+      } catch (error) {
+        alert(error);
+        setLoading(false);
+      }
+    };
+    document.body.appendChild(script);
+  }
+
   return (
     <Container>
       <div className='col-md-4 order-md-2 mb-4'>
@@ -31,15 +94,20 @@ const CartPage = () => {
             <CartItem key={item.id} {...item} />
           ))}
         </ul>
-        <div className='mb-3 fw-bold fs-5 '>
-          Total{' '}
+        <Button
+          variant='dark'
+          className='w-100'
+          onClick={loadRazorpay}
+          disabled={loading}
+        >
+          Pay{' '}
           {formatCurrency(
             cartItems.reduce((total, cartItem) => {
               const item = storeItems.find((i) => i.id === cartItem.id);
               return total + (item?.price || 0) * cartItem.quantity;
             }, 0)
           )}
-        </div>
+        </Button>
       </div>
     </Container>
   );
